409c5d4570936d75cc92e4ce673015ba8d7b21a1
Fhwang <hdh0926@naver.com>
2020-04-12 01:05:05 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreTrackerHelper
+{
+    class Program
+    {
+        public static string getLocation()
+        {
+            string location_x32 = @"C:\Program Files(x86)\CoreTracker";
+            if (Directory.Exists(location_x32)) { return location_x32; }
+            else { return @"C:\Program Files\CoreTracker"; }
+        }
+        static  void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    RefreshTrayArea();
+                    Thread.Sleep(3000);
+                }
+                
+            } catch (Exception e)
+            {
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(getLocation(), "CoreTrackerHelper.error.log")))
+                {
+                    outputFile.WriteLine($"{e.Message}\n\n{e.StackTrace}");
+                }
+                // logging here
+            }
+            
+        }
+        #region "Refresh Notification Area Icons"
+        string name = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass,
+            string lpszWindow);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        public static void RefreshTrayArea()
+        {
+            IntPtr systemTrayContainerHandle = FindWindow("Shell_TrayWnd", null);
+            IntPtr systemTrayHandle = FindWindowEx(systemTrayContainerHandle, IntPtr.Zero, "TrayNotifyWnd", null);
+            IntPtr sysPagerHandle = FindWindowEx(systemTrayHandle, IntPtr.Zero, "SysPager", null);
+            IntPtr notificationAreaHandle = FindWindowEx(sysPagerHandle, IntPtr.Zero, "ToolbarWindow32", "Notification Area");
+            if (notificationAreaHandle == IntPtr.Zero) notificationAreaHandle = FindWindowEx(sysPagerHandle, IntPtr.Zero, "ToolbarWindow32", null);
+            if (notificationAreaHandle == IntPtr.Zero)
+            {
+                notificationAreaHandle = FindWindowEx(sysPagerHandle, IntPtr.Zero, "ToolbarWindow32", "User Promoted Notification Area");
+                IntPtr notifyIconOverflowWindowHandle = FindWindow("NotifyIconOverflowWindow", null);
+                IntPtr overflowNotificationAreaHandle = FindWindowEx(notifyIconOverflowWindowHandle, IntPtr.Zero, "ToolbarWindow32", "Overflow Notification Area");
+                RefreshTrayArea(overflowNotificationAreaHandle);
+            }
+            RefreshTrayArea(notificationAreaHandle);
+        }
+
+        private static void RefreshTrayArea(IntPtr windowHandle)
+        {
+            const uint wmMousemove = 0x0200;
+            RECT rect;
+            GetClientRect(windowHandle, out rect);
+            for (var x = 0; x < rect.right; x += 5)
+                for (var y = 0; y < rect.bottom; y += 5)
+                    SendMessage(windowHandle, wmMousemove, 0, (y << 16) + x);
+        }
+        #endregion
+    }
+}
